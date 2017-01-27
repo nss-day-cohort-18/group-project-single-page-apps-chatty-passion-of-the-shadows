caba6bafbd00e4a593adad8cb153f1a5d8d7542a
Taylor Perkins <taylorperkins@Taylors-MacBook-Pro.local>
2017-01-27 08:25:24 -0600

    Connected all IIFE's fully, and added in comments for everything. Initial functions are working perfectly.

--- js/createTexts.js
+++ js/createTexts.js
@@ -1,15 +1,73 @@
 'use strict';
 
+//SEE README.md UNDER MODULAR CODE FOR MORE DETAILS
+//
+
 var Chatty = (function(createTexts) {
 
+	//===============================//
+	//===========VARIABLES===========//
+	//===============================//
+
+	//Start-up to make sure it works
 	var myText2 = "It's working.";
 
-	createTexts.makeSureItWorks2 = function() {
-		console.log(myText2);
+	//Array to hold ALL text messages created
+	//Used in readTextMessages function
+	var createdTexts = [
+		{
+			id1: "placeholder text input"
+		}
+	];
+
+	//Grabbing HTML elements needed for this function. 
+	var createHtmlElements = {
+		//Actual VALUE of userText input
+		userTextInput: document.getElementById("new-message").value,
+		//Parent Div to attach created messages to
+		messageDisplayWrapper: document.getElementById("message-display"),
+		//HTML Collection of all delete buttons created (IDK if we'll need this)
+		textMessageDeleteButtons: document.getElementsByClassName("delete-button")
 	};
 
-	return createTexts;
 
+	//===============================//
+	//===========FUNCTIONS===========//
+	//===============================//
+
+	//Start-up function tomake sure it's connected.
+	createTexts.makeSureItWorks2 = function() {
+		console.log(myText2, " Location: createTexts.js");
+	};
+
+	//Function to create Text Cards
+	createTexts.createTextMessages = function(elementId, userMessage) {
+		//1. Check if user presses enter key
+		//2. Store userMessage in createdTexts array
+		//3. Create elements and append them
+		//	 to createHtmlElements.messageDisplayWrapper
+		var whatThisDoes = "createTextMessages() works. Location: createTexts.js";
+		console.log(whatThisDoes);
+	};
+
+	//This function reads texts back to you from createdTexts array
+	createTexts.readTextMessages = function(text) {
+		console.log("readTextMessages() works. Location: createTexts.js");
+		//To test: Chatty.readTextMessages("id1");
+		return createdTexts[0][text];
+	};
+
+	createTexts.deleteSingleText = function() {
+		var toCheckIfWorks = "deleteSingleText() works. Location: createTexts.js";
+		console.log(toCheckIfWorks);
+	};
+
+
+	//============================//
+	//===========RETURN===========//
+	//============================//
+
+	return createTexts;
 
 })(Chatty || {});
 
--- js/initialTexts.js
+++ js/initialTexts.js
@@ -1,18 +1,67 @@
 'use strict';
 
-var myMessage = "Hi World";
-console.log(myMessage);
+//SEE README.md UNDER MODULAR CODE FOR MORE DETAILS
+//This function takes JSON file (holds preset messages)
+//and returns the value
 
 var Chatty = (function(initialTexts) {
 
+	//===============================//
+	//===========VARIABLES===========//
+	//===============================//
+
+	//Start-up to make sure it works
 	var myText1 = "It's working.";
 
-	initialTexts.makeSureItWorks = function() {
-		console.log(myText1);
+	//Object that  contains all HTML elements needed 
+	//for this IIFE
+	//Chatty.returnInitialHTMLElements("textMessages");
+	var initialHtmlElements = {
+		textMessages: document.getElementsByClassName("text-message")
 	};
+
+	//JSON format that holds all of our pre-messages.
+	//Chatty.returnPreMessages("message1");
+	var preMessages = [
+		{
+			message1: "Pre-message-1",
+			message2: "Pre-message-2",
+			message3: "Pre-message-3",
+			message4: "Pre-message-4",
+			message5: "Pre-message-5",
+		}
+	];
+
+
+	//===============================//
+	//===========FUNCTIONS===========//
+	//===============================//
+
+	//Start-up function tomake sure it's connected.
+	initialTexts.makeSureItWorks = function() {
+		console.log(myText1, " Location: initialTexts.js");
+	};
+
+	//Test function to target id#text-message
+	initialTexts.returnInitialHtmlElements = function(htmlElement) {
+		console.log("returnInitialHtmlElements() works. Location: initialTexts.js");
+		//To test: Chatty.returnInitialHtmlElements["textMessages"];
+		return initialHtmlElements[htmlElement];
+	};
+
+	//Function that returns pre-saved messages
+	//Goes inside first 5 "text-messages"
+	initialTexts.returnPreMessages = function(word) {
+		console.log("returnPreMessages() works. Location: initialTexts.js");
+		//To test: Chatty.returnPreMessages("message1");
+		return preMessages[0][word];
+	};
+
+
+	//============================//
+	//===========RETURN===========//
+	//============================//
 
 	return initialTexts;
 
-
 })(Chatty || {});
-
