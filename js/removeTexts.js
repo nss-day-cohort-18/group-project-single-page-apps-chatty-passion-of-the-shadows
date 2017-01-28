f42c4ff1136133c1e747e962d2c211ff62444649
Taylor Perkins <taylorperkins@Taylors-MacBook-Pro.local>
2017-01-28 10:57:03 -0600

    Re-worked the if statements to match all possibilities of the clear-all button being visible or not.

--- js/removeTexts.js
+++ js/removeTexts.js
@@ -34,19 +34,31 @@ var Chatty = (function(removeTexts) {
 	};
 
 	removeTexts.deleteClick = function () {
+		//Grabbing target
 		var elementClicked = event.target;
+		//Grabbing wrapper to remove elements from
 		var wrapper = document.getElementById("message-display");
+		//If it is a delete button within a text..
 		if (elementClicked.classList.contains("delete-button")) {
+			//remove the entire card
 			elementClicked.parentElement.remove();
+			//if there are no more cards.. 
+			if (wrapper.children.length === 0) {
+				//Give the clear all button a class of hide
+				document.getElementById("clear-button").classList.add("hide");
+			}
+		//If the event.target is the clear all button
 		} else if (elementClicked.id ==="clear-button") {
+			//If the wrapper element has children
 			if (wrapper.hasChildNodes()) {
-	            elementClicked.classList.toggle("hide");
+				//add the class of hide to clear-all button 
+	            elementClicked.classList.add("hide");
+	            //While there are text-cards, remove them
 	            while (wrapper.firstChild) {
 	                wrapper.removeChild(wrapper.firstChild);
 	            }
 	        }
-		};
-
+		}
 	};
 
 
