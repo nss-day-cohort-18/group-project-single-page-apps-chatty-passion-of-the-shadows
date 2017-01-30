abcf4be7a6a1f777e2aae4153a4f63d4e2b9f34c
Zak Spence <zakspence@gmail.com>
2017-01-30 11:40:57 -0600

    bug fixin

--- js/toggle.js
+++ js/toggle.js
@@ -4,28 +4,40 @@
 function toggleStyles(){
 	var elementsToToggle = {
 
-	 	messageCard		: document.getElementById("message-card"),
-	 	deleteButton	: document.getElementById("delete-button"),
+	 	messageCard		: document.getElementsByClassName("message-card"),
+	 	deleteButton	: document.getElementsByClassName("delete-button"),
 	 	messageDisplay	: document.getElementById("message-display"),
 	 	messageWrapper	: document.getElementById("message-wrapper"),
 	 	navElement		: document.getElementById("nav-element"),
 	 	clearButton		: document.getElementById("clear-button"),
 	 	newMessage      : document.getElementById("new-message"),
-	 	largeText		: document.getElementById("text-message")
+	 	// largeText		: document.getElementsByClassName("text-message")
 	};
 
 	for (element in elementsToToggle) {
+		var currentElement = elementsToToggle[element];
+		console.log('current element in loop:', currentElement)
+		console.log('is array?', Array.isArray(currentElement));
+		if (!Array.isArray(currentElement)) {
+			for (var i = 0; i < currentElement.length; i++) {
+				var className = currentElement.item(i).className;
+				console.log(className)
+				currentElement.item(i).classList.toggle(`${className}-dark`);
+			} 
+		} else {
 		// We grab the id, because we're gonna use it in each
 		// class name for the dark theme.
 		//
 		console.log('--stuff in our loop--');
-		console.log('togglingElements')
-		var id = element.id;
-		console.log(id)
+		console.log('Toggling Elements:', elementsToToggle);
+		var id = currentElement.id;
+		console.log(id);
 
 		// Now we toggle the theme from light to dark.
 		// 
-		console.log(element.classList);
-		element.classList.toggle(`${id}-dark`);
+		console.log(currentElement.classList);
+		currentElement.classList.toggle(`${id}-dark`);
+		}
+	console.log(elementsToToggle)
 	}
 }
